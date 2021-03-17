e0049e06e909312836822ab401cfe390ad85a567
Passos <66309118+GabrielPPassos@users.noreply.github.com>
2021-03-16 23:30:32 -0300

    otimizacao

--- farms/vinho/passos_vinho-entregas/client-side/main.lua
+++ farms/vinho/passos_vinho-entregas/client-side/main.lua
@@ -12,14 +12,15 @@ local pedido = 0
 
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(1)
+		local otimizacao = 1000
 		if not entregando then
 			local ped = PlayerPedId()
 			local x,y,z = table.unpack(GetEntityCoords(ped))
             local CoordenadaX,CoordenadaY,CoordenadaZ = -313.01, -1032.11, 31.34
 			local bowz,cdz = GetGroundZFor_3dCoord(CoordenadaX,CoordenadaY,CoordenadaZ)
 			local distance = GetDistanceBetweenCoords(CoordenadaX,CoordenadaY,cdz,x,y,z,true)
-			if distance <= 30.0 then
+			if distance <= 3.0 then
+				otimizacao = 1
 				DrawMarker(21,CoordenadaX,CoordenadaY,CoordenadaZ-0.5,0,0,0,0,180.0,130.0,1.0,1.0,0.5,240,0,0,30,1,0,0,1)
 				if distance <= 1.5 then
 					drawTxt("PRESSIONE  ~b~E~w~  PARA INICIAR A ROTA DE VINHO",4,0.5,0.93,0.50,255,255,255,180)
@@ -32,19 +33,21 @@ Citizen.CreateThread(function()
 				end
 			end
 		end
+		Citizen.Wait(otimizacao)
 	end
 end)
 
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(1)
+		local otimizacao = 1000
 		if entregando then
 			local before 
 			local ped = PlayerPedId()
 			local x,y,z = table.unpack(GetEntityCoords(ped))
 			local bowz,cdz = GetGroundZFor_3dCoord(config.entregas[selecionado].x,config.entregas[selecionado].y,config.entregas[selecionado].z)
 			local distance = GetDistanceBetweenCoords(config.entregas[selecionado].x,config.entregas[selecionado].y,cdz,x,y,z,true)
-			if distance <= 30.0 then
+			if distance <= 3.0 then
+				otimizacao = 1
 				DrawMarker(21,config.entregas[selecionado].x,config.entregas[selecionado].y,config.entregas[selecionado].z-0.7,0,0,0,0,180.0,130.0,1.0,1.0,0.5,240,200,80,30,1,0,0,1)
 				if distance <= 1.5 then
 					drawTxt("PRESSIONE  ~r~E~w~  PARA ENTREGAR ~r~".. pedido .." VINHO(S)",4,0.5,0.93,0.50,255,255,255,180)
@@ -59,20 +62,22 @@ Citizen.CreateThread(function()
 									selecionado = selecionado + 1
 								end
 								cblip(entregas,selecionado)
-						else
-							entregando = false
+							else
+								entregando = false
 							end
+						else
+							TriggerClientEvent("Notify", "negado", "Não encontramos nenhum <b>vinho</b> na sua mochila.")
 						end
 			        end
                 end
             end
         end
+		Citizen.Wait(otimizacao)
 	end
 end)
 
 Citizen.CreateThread(function()
 	while true do
-        Citizen.Wait(1)
         if entregando then
 		    drawTxt("~c~PARA CANCELAR AS ENTREGAS PRESSIONE ~r~[F7]",4,0.270,0.905,0.45,255,255,255,200)
 		    drawTxt("VOCÊ PRECISA ENTREGAR ~r~".. pedido .." VINHO(S)",4,0.270,0.93,0.45,255,255,255,200)
@@ -82,13 +87,15 @@ end)
 
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(1)
+		local otimizacao = 1000
 		if entregando then
+			otimizacao = 1
 			if IsControlJustPressed(0,168) then
 				entregando = false
 				RemoveBlip(blips)
 			end
 		end
+        Citizen.Wait(otimizacao)
 	end
 end)
 
